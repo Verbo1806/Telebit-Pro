eac151c296fb5f59b8905a5cb1b59a60823be2ec
Verbo1806 <sashoverbovskiy@gmail.com>
2017-07-03 13:54:52 +0300

    First day 1.1

--- Project/Assets/Script/Menu/Ads/Ads.cs
+++ Project/Assets/Script/Menu/Ads/Ads.cs
@@ -1,35 +1,49 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.Advertisements;
 
-public class Ads : MonoBehaviour {
-	public int game_num;
+public class Ads : MonoBehaviour
+{
+
 	public int coins;
 	public GameObject button;
-	
-	// Use this for initialization
-	void Start () {
 
-		Advertisement.Initialize ("1360539", true);
-	}
-	
-	void Update(){
+	void Update() {
 
-		coins = PlayerPrefs.GetInt ("coin");
+		coins = PlayerPrefs.GetInt ("coin", 0);
 	}
-	
-	public void Click() {
-		if(Advertisement.IsReady()){
-			Advertisement.Show();
-			Destroy (button);
-			game_num = 0;
-			PlayerPrefs.SetInt("game_num", game_num);
-			PlayerPrefs.Save();
-			coins += 10;
-			PlayerPrefs.SetInt("coin", coins);
-			PlayerPrefs.Save();
+
+	public void ShowRewardedAd()
+	{
+		if (Advertisement.IsReady("rewardedVideo"))
+		{
+			var options = new ShowOptions { resultCallback = HandleShowResult };
+			Advertisement.Show("rewardedVideo", options);
 		}
-		
 	}
-	
+
+	private void HandleShowResult(ShowResult result)
+	{
+		switch (result)
+		{
+		case ShowResult.Finished:
+			Debug.Log ("The ad was successfully shown.");
+
+			coins += 10;
+			if (coins > 999)
+				coins = 999;
+
+			PlayerPrefs.SetInt ("coin", coins);
+			PlayerPrefs.Save ();
+
+			Destroy(button);
+
+			break;
+		case ShowResult.Skipped:
+			Debug.Log("The ad was skipped before reaching the end.");
+			break;
+		case ShowResult.Failed:
+			Debug.LogError("The ad failed to be shown.");
+			break;
+		}
+	}
 }
---
+++ Project/Assets/Script/Menu/Buttons/Buttons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+
+	// Use this for initialization
+	public void Level () {
+
+		Application.LoadLevel (3);
+	}
+	
+	// Update is called once per frame
+	public void Upgrade () {
+
+		Application.LoadLevel (2);
+	}
+}

6854bed8f92cbcad8dc2fba1dda745e7c520e67d
Kroxitrock <kroxitrock@gmail.com>
2017-07-03 12:27:34 +0300

    Working Player

---
+++ Project/Assets/Script/Level/Player/Character_Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Movement : MonoBehaviour {
+	
+	private float jump;
+	private bool canJump;
+	public float moveSpeed;
+	public float jumpHeight;
+	
+	// Update is called once per frame
+	protected float OnMouseDown()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			return (float)1;
+		}
+		return (float)0;
+	}
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if (coll.collider.tag == "Floor") {
+            canJump = true;
+        }
+	}
+    void Jump()
+    {
+        if (canJump == true)
+        {
+            jump = OnMouseDown();
+            canJump = false;
+        }
+    }
+    void run()
+    {
+        
+        Jump();
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(moveSpeed, jumpHeight * jump), ForceMode2D.Impulse);
+    }
+	void FixedUpdate()
+	{
+        run();		
+	}
+}

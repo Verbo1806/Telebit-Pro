8f10f2ae8245ee4fd1840440c73cae9e2e0db33f
Kroxitrock <kroxitrock@gmail.com>
2017-07-03 15:55:30 +0300

    First day 1.3

--- Project/Assets/Script/Level/Player/Character_Movement.cs
+++ Project/Assets/Script/Level/Player/Character_Movement.cs
@@ -8,9 +8,15 @@ public class Character_Movement : MonoBehaviour {
 	private bool canJump;
 	public float moveSpeed;
 	public float jumpHeight;
-	
-	// Update is called once per frame
-	protected float OnMouseDown()
+    int doublejump;
+    bool isDoubleJumping;
+    private void Start()
+    {
+        doublejump = PlayerPrefs.GetInt("DOUBLEJUMP", 0);
+        if (doublejump == 1) isDoubleJumping = true;
+    }
+    // Update is called once per frame
+    protected float OnMouseDown()
 	{
 		if (Input.GetMouseButtonDown(0))
 		{
@@ -18,32 +24,44 @@ public class Character_Movement : MonoBehaviour {
 		}
 		return (float)0;
 	}
-
     void OnCollisionStay2D(Collision2D coll)
     {
         if (coll.collider.tag == "Floor") {
             canJump = true;
+            if(doublejump == 1) isDoubleJumping = true;
         }
 	}
-
     void Jump()
     {
         if (canJump == true)
         {
             jump = OnMouseDown();
             canJump = false;
-        }
-    }
 
+            Debug.Log("canJump");
+        }
+        else if (isDoubleJumping)
+        {
+            Debug.Log("isDoubleJumping");
+            jump = OnMouseDown();
+            if (jump == (float)1)
+            {
+                isDoubleJumping = false;
+                jump = (float)0.8;
+            }
+        }
+        else jump = 0;
+        
+    }
     void run()
     {
-        
         Jump();
+        Debug.Log("Run");
         GetComponent<Rigidbody2D>().AddForce(new Vector2(moveSpeed, jumpHeight * jump), ForceMode2D.Impulse);
     }
-
 	void FixedUpdate()
 	{
-        run();		
+        run();
+        Debug.Log(jump);
 	}
 }

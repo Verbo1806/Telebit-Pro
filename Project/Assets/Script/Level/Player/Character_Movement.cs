fdbc9e0b74e407dd66e6716552d3c9fabf5b83d4
Kroxitrock <kroxitrock@gmail.com>
2017-07-04 12:08:19 +0300

    Second day 1.0

--- Project/Assets/Script/Level/Player/Character_Movement.cs
+++ Project/Assets/Script/Level/Player/Character_Movement.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character_Movement : MonoBehaviour {
-	
+
+    Rigidbody2D Player;
+
 	private float jump;
 	private bool canJump;
-	public float moveSpeed;
+
+
+    public float moveSpeed;
 	public float jumpHeight;
+
+    public float flySpeed;
+    public float flyHeight;
+    public int waitTimme;
+
     int doublejump;
     bool isDoubleJumping;
+
+
+    public static bool flying;
+    public static bool running;
+
     private void Start()
     {
+        Player = GetComponent<Rigidbody2D>();
+        running = true;
         doublejump = PlayerPrefs.GetInt("DOUBLEJUMP", 0);
         if (doublejump == 1) isDoubleJumping = true;
     }
-    // Update is called once per frame
+
     protected float OnMouseDown()
 	{
 		if (Input.GetMouseButtonDown(0))
@@ -24,6 +40,8 @@ public class Character_Movement : MonoBehaviour {
 		}
 		return (float)0;
 	}
+
+
     void OnCollisionStay2D(Collision2D coll)
     {
         if (coll.collider.tag == "Floor") {
@@ -31,6 +49,8 @@ public class Character_Movement : MonoBehaviour {
             if(doublejump == 1) isDoubleJumping = true;
         }
 	}
+
+
     void Jump()
     {
         if (canJump == true)
@@ -38,11 +58,11 @@ public class Character_Movement : MonoBehaviour {
             jump = OnMouseDown();
             canJump = false;
 
-            Debug.Log("canJump");
+            //Debug.Log("canJump");
         }
         else if (isDoubleJumping)
         {
-            Debug.Log("isDoubleJumping");
+            //Debug.Log("isDoubleJumping");
             jump = OnMouseDown();
             if (jump == (float)1)
             {
@@ -53,15 +73,37 @@ public class Character_Movement : MonoBehaviour {
         else jump = 0;
         
     }
+
+
     void run()
     {
         Jump();
-        Debug.Log("Run");
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(moveSpeed, jumpHeight * jump), ForceMode2D.Impulse);
+        //Debug.Log("Run");
+        Player.AddForce(new Vector2(moveSpeed, jumpHeight * jump), ForceMode2D.Impulse);
     }
+    IEnumerator Wait(int sec)
+    {
+        
+        yield return new WaitForSeconds(sec);
+    }
+
+    void fly()
+    {
+        Debug.Log("Flying");
+        Player.AddForce(new Vector2(0, flyHeight), ForceMode2D.Impulse);
+        Wait(waitTimme);
+        Player.AddForce(new Vector2(flySpeed, 0), ForceMode2D.Impulse);
+        flying = false;
+        running = true;
+    }
+
+
 	void FixedUpdate()
 	{
-        run();
-        Debug.Log(jump);
+        if(running)
+            run();
+        if(flying)
+            fly();
+        //Debug.Log(jump);
 	}
 }

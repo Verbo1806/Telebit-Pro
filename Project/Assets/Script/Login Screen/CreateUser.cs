0e4527843f148497136bc61a14d4cf6570da1747
Kroxitrock <kroxitrock@gmail.com>
2017-07-06 09:59:34 +0300

    Working Registrration

---
+++ Project/Assets/Script/Login Screen/CreateUser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateUser : MonoBehaviour {
+
+	public void onclick()
+    {
+        if (GetEmail.Email != null && GetUsername.Username != null && GetPassword.Pass != null)
+        {
+            InformationInserter.CreateUser(GetEmail.Email, GetUsername.Username, GetPassword.Pass);
+            Application.LoadLevel(1);
+        }
+    }
+}
---
+++ Project/Assets/Script/Login Screen/InformationInserter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InformationInserter : MonoBehaviour {
+    public string inputEmail;
+    public string inputUsername;
+    public string inputPassword;
+
+
+    static string insertInfoURL = "http://localhost/TelebidProject/InsertData.php";
+    
+    public static void CreateUser(string email, string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("email", email);
+        form.AddField("pass", password);
+        form.AddField("coins", 0);
+        form.AddField("speed", 0);
+        form.AddField("time", 0);
+        form.AddField("doublejump", 0);
+        form.AddField("death", 0);
+        form.AddField("fly", 0);
+
+        WWW www = new WWW(insertInfoURL, form);
+    }
+}
